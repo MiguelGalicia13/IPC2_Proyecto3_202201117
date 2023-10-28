67c2dd21b90b3c513f58898a87ecdbf63668448b
Mike PC <3776080760101@ingenieria.usac.edu.gt>
2023-10-28 07:09:20 -0600

    Conexion Frontend-Backend

--- frontend/mydjangoapp/myapp/urls.py
+++ frontend/mydjangoapp/myapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('myform/',views.myform,name='myform')
+    path('myform/',views.myform_view,name='myform')
     ]
---
+++ frontend/mydjangoapp/myapp/views.py
@@ -0,0 +1,26 @@
+import requests
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def myform_view(request):
+    if request.method == 'POST':
+        data = request.POST.get("data")
+        file = request.FILES.get("file")
+
+        if not file:
+            return JsonResponse({"message": "No se ha seleccionado un archivo."})
+
+        try:
+            # Envía la solicitud al backend de Flask con el archivo adjunto
+            files = {"file": (file.name, file.read())}
+            response = requests.post('http://127.0.0.1:5000/grabar_mensaje', data={"data": data}, files=files)
+            response.raise_for_status()
+
+            # Procesa la respuesta del backend de Flask
+            response_data = response.json()
+            return JsonResponse(response_data)
+        except requests.exceptions.RequestException as e:
+            return HttpResponse(str(e), status=500)
+
+    return render(request, 'myform.html')
